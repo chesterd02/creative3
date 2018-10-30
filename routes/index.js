581be0b59a026a5bc490d656196d41e75b4a6d07
Chester Davison <chesterd02@gmail.com>
2018-10-30 22:27:34 +0000

    changed name and removed url

--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,22 @@ var express = require('express');
 var router = express.Router();
 var pokemon = [
   {
-    name: 'Pikachu',
-    avatarUrl: 'http://rs795.pbsrc.com/albums/yy232/PixKaruumi/Pokemon%20Pixels/Pikachu_Icon__free__by_Aminako.gif~c200'
+    name: 'Message1',
   },
   {
-    name: 'Charmander',
-    avatarUrl: 'http://24.media.tumblr.com/tumblr_ma0tijLFPg1rfjowdo1_500.gif'
-
+    name: 'Message2',
   },
   {
-    name: 'Mew',
-    avatarUrl: 'http://media3.giphy.com/media/J5JrPT8r1xGda/giphy.gif'
+    name: 'Message3',
   },
   {
-    name: 'Cubone',
-    avatarUrl: 'http://rs1169.pbsrc.com/albums/r511/nthndo/tumblr_ljsx6dPMNm1qii50go1_400.gif~c200'
+    name: 'Message4',
   },
   {
-    name: 'Cleffa',
-    avatarUrl: 'http://media1.giphy.com/media/pTh2K2xTJ1nag/giphy.gif'
+    name: 'Message5',
   },
   {
-    name: 'Gengar',
-    avatarUrl: 'https://s-media-cache-ak0.pinimg.com/originals/7e/3b/67/7e3b67c53469cc4302035be70a7f2d60.gif'
+    name: 'Message6',
   }
 ];
 /* GET home page. */
